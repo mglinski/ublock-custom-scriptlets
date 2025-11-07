dfed5c846e2cbd8ddeceba0ebe6d02fd9fef6526
Matthew Glinski <matthewglinski@gmail.com>
2025-11-06 21:59:32 -0500

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -6,14 +6,12 @@ function callFunctionDelayed(
     funcCall = '',
     waitTime = 0
 ) {
-
     // @link: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number#answer-175787
     const isStrNumeric = (str) => {
         if (typeof str != "string") return false; // we only process strings!  
         // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this) and ensure strings of whitespace fail
         return !isNaN(str) && !isNaN(parseFloat(str));
     };
-    
     if (funcCall === '') {
         console.log(`uBO-Scriptlet[callFunctionDelayed]: ERROR: funcCall is empty string`);    
         return;
@@ -22,9 +20,7 @@ function callFunctionDelayed(
         console.log(`uBO-Scriptlet[callFunctionDelayed]: ERROR: waitTime is not a valid number`);
         return;
     }
-    
     waitTime = parseInt(waitTime);
-    
     const funcInvoke = async () => {
         const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
         try {
