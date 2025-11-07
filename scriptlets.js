ca037565dddc64606ac549f066e90b5b5ad2e864
Matthew Glinski <matthewglinski@gmail.com>
2025-11-06 22:33:45 -0500

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -9,7 +9,6 @@ function callFunctionDelayed(
 ) {
     const safe = safeSelf();
     const logPrefix = safe.makeLogPrefix('call-function-delayed', funcCall, waitTime);
-    
     // @link: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number#answer-175787
     const isStrNumeric = (str) => {
         if (typeof str !== "string") return false; // we only process strings!  
