aee5262d249cfae068346694bcf537d9752b260f
Matthew Glinski <matthewglinski@gmail.com>
2025-11-06 22:29:45 -0500

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -1,11 +1,15 @@
 /// call-function-delayed.js
 /// alias cfd.js
 /// dependency run-at.fn
+/// dependency safe-self.fn
 // example.com##+js(cfd, funcName, waitTime)
 function callFunctionDelayed(
     funcCall = '',
     waitTime = 0
 ) {
+    const safe = safeSelf();
+    const logPrefix = safe.makeLogPrefix('call-function-delayed', funcCall, waitTime);
+    
     // @link: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number#answer-175787
     const isStrNumeric = (str) => {
         if (typeof str !== "string") return false; // we only process strings!  
@@ -13,25 +17,27 @@ function callFunctionDelayed(
         return !isNaN(str) && !isNaN(parseFloat(str));
     };
     if (funcCall === '') {
-        console.error(`uBO-Scriptlet[callFunctionDelayed]: ERROR: funcCall is empty string`);    
+        safe.uboErr(logPrefix, `Error: funcCall is empty string`);
         return;
     }
     if (typeof waitTime === "string" && !isStrNumeric(waitTime)) {
-        console.error(`uBO-Scriptlet[callFunctionDelayed]: ERROR: waitTime is not a valid number`);
+        safe.uboErr(logPrefix, `Error: waitTime is not a valid number`);
         return;
     }
     waitTime = parseInt(waitTime);
     const funcInvoke = async () => {
         const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
         try {
-            console.log(`uBO-Scriptlet[callFunctionDelayed]: waiting ${waitTime} ms...`);
-            if (waitTime > 0) await sleep(waitTime);
-            console.log(`uBO-Scriptlet[callFunctionDelayed]: calling -> ${funcCall}`);
+            if (waitTime > 0) {
+                if ( safe.logLevel > 1) safe.uboLog(logPrefix, `waiting ${waitTime} ms...`);
+                await sleep(waitTime);
+            }
             const fn = window[funcCall];
+            if ( safe.logLevel > 1) safe.uboLog(logPrefix, `started calling window.${funcCall}()`);
             self.requestAnimationFrame(fn);
-            console.log(`uBO-Scriptlet[callFunctionDelayed]: called -> ${funcCall}`);
+            if ( safe.logLevel > 1) safe.uboLog(logPrefix, `finished calling window.${funcCall}()`);
         } catch (e) {
-            console.error(`uBO-Scriptlet[callFunctionDelayed]: EXCEPTION`, e);
+            safe.uboErr(logPrefix, `Error: ${e}`);
         }
     };
     runAt(() => {
